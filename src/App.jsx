b5faa0df87bbe1fa31345745051e99e183f2c8da
Pooja jat <patelpoojajat@gmail.com>
2025-09-23 00:31:40 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
 import CardContainer from "./components/CardContainer";
